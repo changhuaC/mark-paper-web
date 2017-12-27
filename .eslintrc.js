5c0098d5dbada7505df751ce950b7b3b75864e4a
changhuaC <303713829@qq.com>
2017-12-27 23:30:01 +0800

    初始化
    第一次初始化

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "extends": "standard",
+    "rules": {
+        "no-const-assign": "warn",
+        "no-this-before-super": "warn",
+        "no-undef": "warn",
+        "no-unreachable": "warn",
+        "no-unused-vars": "warn",
+        "constructor-super": "warn",
+        "valid-typeof": "warn",
+        "indent": ["error", 4]
+    },
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+};
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack')
+const pathTool = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const distPath = pathTool.resolve(__dirname, 'dist') // 根目录路径
+const templatePath = pathTool.resolve(__dirname, 'template') // 模版目录路径
+
+module.export = {
+    devtool: 'source-map',
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: distPath
+    },
+    module: {
+        rules: [
+            // 加载js文件，是用babel转码
+            {
+                test: /.js$/,
+                use: ['babel-loader'],
+                exclude: /node_modules/,
+                option: {
+                    preset: ['@babel/preset-env']
+                }
+            },
+            // 加载样式文件，转换提取内部样式
+            {
+                test: /.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            // 根据文件路径加载图片文件
+            {
+                test: /.(png|svg|jpg|gif|)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            // 加载字体文件
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ['file-loader']
+            }
+        ]
+    },
+    devServer: {
+        contentBase: distPath,
+        historyApiFallback: true,
+        inline: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '登录',
+            template: pathTool.resolve(templatePath, 'tmp-index.html'),
+            filename: 'index.html',
+            hash: true
+        })
+    ]
+}
