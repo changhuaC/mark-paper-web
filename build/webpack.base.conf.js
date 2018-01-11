0e21abf7fa4f389bffa8e7e0bcf379fdcc3ae27f
changhua <changh@seaskylight.com>
2018-01-11 17:33:41 +0800

    修改配置

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -4,10 +4,43 @@ import path from 'path'
 import devConfig from '../config/dev-env.conf'
 import prodConfig from '../config/prod-env.conf'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
+const isProduction = process.env.NODE_ENV === 'production'
+const useSourceMap = isProduction ? prodConfig.productionSourceMap : devConfig.cssSourceMap
 const contextPath = path.resolve(__dirname, '../') // 基础目录，绝对路径，用于从配置中解析入口起点(entry point)和 loader
 const distPath = path.resolve(__dirname, 'dist') // 根目录路径
 const templatePath = path.resolve(__dirname, 'template') // 模版目录路径
+const srcPath = path.join(__dirname, '..', 'src') // 源码路径
+
+// load生成器
+function generateLoaders (loader, loaderOptions) {
+    const loaders = [{
+        loader: 'css-loader',
+        options: {
+            sourceMap: useSourceMap
+        }
+    }]
+
+    if (loader) {
+        loaders.push({
+            loader: loader + '-loader',
+            options: Object.assign({}, loaderOptions, {
+                sourceMap: useSourceMap
+            })
+        })
+    }
+    // 将样式提取成一个独立的文件
+    // 在打包生产的时候
+    if (isProduction) {
+        return ExtractTextPlugin.extract({
+            use: loaders,
+            fallback: 'vue-style-loader'
+        })
+    } else {
+        return ['vue-style-loader'].concat(loaders)
+    }
+}
 
 export default {
     context: contextPath,
@@ -17,7 +50,15 @@ export default {
     },
     output: {
         filename: '[name].js',
-        path: prodConfig.assetsRoot
+        path: prodConfig.assetsRoot,
+        publicPath: isProduction ? prodConfig.assetsPublicPath : devConfig.assetsPublicPath
+    },
+    resolve: {
+        extensions: ['.vue', '.json', '.js'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': path.join(__dirname, '..', 'src')
+        }
     },
     module: {
         rules: [
@@ -25,7 +66,8 @@ export default {
             {
                 test: /.js$/,
                 use: ['babel-loader'],
-                exclude: /node_modules/,
+                // exclude: /node_modules/,
+                include: [srcPath],
                 option: {
                     preset: ['@babel/preset-env']
                 }
@@ -35,6 +77,23 @@ export default {
                 exclude: /(node_modules)/,
                 use: {
                     loader: 'vue-loader'
+                },
+                options: {
+                    loaders: {
+                        css: generateLoaders(),
+                        postcss: generateLoaders(),
+                        less: generateLoaders('less'),
+                        sass: generateLoaders('sass', { indentedSyntax: true }),
+                        scss: generateLoaders('sass')
+                    },
+                    cssSourceMap: useSourceMap,
+                    cacheBusting: devConfig.cacheBusting,
+                    transformToRequire: {
+                        video: ['src', 'poster'],
+                        source: 'src',
+                        img: 'src',
+                        image: 'xlink:href'
+                    }
                 }
             },
             // 加载样式文件，转换提取内部样式
@@ -44,10 +103,12 @@ export default {
             },
             // 根据文件路径加载图片文件
             {
-                test: /.(png|svg|jpg|gif|)$/,
-                use: [
-                    'file-loader'
-                ]
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                use: ['url-loader'],
+                options: {
+                    limit: 10000,
+                    name: utils.assetsPath('img/[name].[hash:7].[ext]')
+                }
             },
             // 加载字体文件
             {
